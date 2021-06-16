a1b0f98fb3bea8accc753a53549779864fef7ae5
Hung Tang <hungtang@hotmail.com>
2021-06-16 15:20:19 -0700

    creating components baseline

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import ContactForm from './components/Contact';
+import Nav from './components/Nav';
+
 
 function App() {
+
+  const [categories] = useState([
+    {
+      name: 'About' },
+    { name: 'portfolio' },
+    { name: 'resume' },
+    { name: 'contact' }
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  const [contactSelected, setContactSelected] = useState(false);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav 
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+      />
+      <main>
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory} />
+            <About />
+          </>
+        ) : (
+          <ContactForm />
+        )}
+      </main>
     </div>
   );
 }
